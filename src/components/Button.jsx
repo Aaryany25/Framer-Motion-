fdb82e13d475a4f0436b578b6e4f00beef8da904
Aaryan <aaryany2505@gmail.com>
2026-01-02 10:37:26 +0530

    Made Button using WhileHover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { motion } from 'motion/react'
 function Button() {
   return (
-    <motion.div initial={{
-        rotate:0,
+  
+    <motion.div 
+      whileHover={{
+        rotateX:25,
+        rotateY:10,
+        y:-5,
+        boxShadow:"0px 20px 50px rgba(0,0,0,0.7)"
       }}
-      animate={{
-        rotate:[0,10,-10,0]
+      whileTap={{
+        y:5
       }}
       transition={{
-        duration:2
+        duration:0.5
       }} className=' bg-black text-white flex items-center justify-center px-4 py-2 rounded-xl'>
       <button >Click Me Daddy!!</button>
     </motion.div>
